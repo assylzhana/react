02bb124054a8543de00850e79f01dcec339adc19
assylzhana <kabibullaasylzhan@gmail.com>
2024-02-27 22:04:50 +0600

    added 15 files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar.jsx';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+       
+      </div>
+    </Router>
+  );
+}
+
+export default App;
